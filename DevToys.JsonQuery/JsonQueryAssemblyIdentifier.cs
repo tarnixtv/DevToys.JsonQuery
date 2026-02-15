f3d2b7ea2eb3e592ad399c2ecdcfe26454198479
Tarnix <tarnixtv@gmail.com>
2026-02-15 21:18:04 +0100

    Initial commit

---
+++ DevToys.JsonQuery/JsonQueryAssemblyIdentifier.cs
@@ -0,0 +1,23 @@
+﻿using DevToys.Api;
+
+using System.ComponentModel.Composition;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace DevToys.JsonQuery;
+
+[Export(typeof(IResourceAssemblyIdentifier))]
+[Name(nameof(JsonQueryAssemblyIdentifier))]
+internal sealed class JsonQueryAssemblyIdentifier : IResourceAssemblyIdentifier
+{
+    public ValueTask<FontDefinition[]> GetFontDefinitionsAsync()
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+        string resourceName = "DevToys.JsonQuery.Assets.jq-icon.ttf";
+
+        Stream stream = assembly.GetManifestResourceStream(resourceName)!;
+
+        return new ValueTask<FontDefinition[]>([new("jq-icon", stream)]);
+    }
+}
